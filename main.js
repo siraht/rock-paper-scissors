baaa810a6e5916d12bb4f60f8eae9ecab5e54440
Travis Hinton <t@travishinton.us>
2021-09-14 17:25:32 -0600

    initial file creation and pseudocoding

---
+++ main.js
@@ -0,0 +1,20 @@
+/* Psuedocode
+        Create variable for user choice
+        Prompt user to pick rock, paper, or scissors
+        Create variable for computer choice
+        Randomly choose r, p, or s for computer
+        If user = r
+            If computer = r then tie
+            If computer = p then lose
+            If computer = s then win
+        If user = p
+            If computer = r then win
+            If computer = p then tie
+            If computer = s then lose
+        If user = s 
+            If computer = r then lose
+            If computer = p then tie
+            If computer = s then win
+        Log winner to console
+*/
+
