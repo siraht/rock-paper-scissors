570a0c7e5e0fc990338c0c91e9b033f9080989a8
Travis Hinton <t@travishinton.us>
2021-09-14 19:01:52 -0600

    First draft of 5 round game

--- main.js
+++ main.js
@@ -18,49 +18,69 @@
         Log winner to console
 */
 
-let computerSelection = () => {
-    let number = Math.floor(Math.random()*3);
-    if (number === 0) {
-        return 'rock';
-    } else if (number === 1) {
-        return 'paper';
-    } else
-        return 'scissors';
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let ties = 0;
+    for (let i = 0; i <5; i++) {
+        let computerSelection = () => {
+            let number = Math.floor(Math.random()*3);
+            if (number === 0) {
+                return 'rock';
+            } else if (number === 1) {
+                return 'paper';
+            } else
+                return 'scissors';
+        }
+        
+        function playerSelection() {
+            let choice = prompt('Choose your weapon', 'Rock');
+            return choice.toLowerCase();
+        }
+        
+        let playRound = (player, computer) => {
+            if (player === 'rock') {
+                if (computer === 'rock') {
+                    return 'Rock and rock! It\'s a tie!'
+                } else if (computer === 'paper') {
+                    return 'You lose! Paper beats rock!';
+                } else {
+                    return 'You win! Rock beats scissors!'
+                }
+            }
+            if (player === 'paper') {
+                if (computer === 'rock') {
+                    return 'You win! Paper beats rock!'
+                } else if (computer === 'paper') {
+                    return 'Paper and paper! It\'s a tie!'
+                } else {
+                    return 'You lose! Scissors beat paper!'
+                }
+            }
+            if (player === 'scissors') {
+                if (computer === 'rock') {
+                    return 'You lose! Rock beats scissors!'
+                } else if (computer === 'paper') {
+                    return 'You win! Scissors beat paper'
+                } else {
+                    return 'Scissors and scissors! It\'s a tie!'
+                }
+            }
+        }        
+
+        function scoreChange(playerScore, computerScore) {
+            if (playRound(playerSelection(), computerSelection()).includes('win')) {
+                playerScore = playerScore + 1;
+            } else if (playRound(playerSelection(), computerSelection()).includes('lose')) {
+                computerScore = computerScore + 1;
+            } else {
+                ties = ties + 1;
+            }
+        }
+
+        console.log(playRound(playerSelection(), computerSelection()));
+        console.log(`The score is ${playerScore} to ${computerScore}`);
+    }
 }
 
-function playerSelection() {
-    let choice = prompt('Choose your weapon', 'Rock');
-    return choice.toLowerCase();
-}
-
-let playRound = (player, computer) => {
-    if (player === 'rock') {
-        if (computer === 'rock') {
-            return 'Rock and rock! It\'s a tie!'
-        } else if (computer === 'paper') {
-            return 'You lose! Paper beats rock!';
-        } else {
-            return 'You win! Rock beats scissors!'
-        }
-    }
-    if (player === 'paper') {
-        if (computer === 'rock') {
-            return 'You win! Paper beats rock!'
-        } else if (computer === 'paper') {
-            return 'Paper and paper! It\'s a tie!'
-        } else {
-            return 'You lose! Scissors beat paper!'
-        }
-    }
-    if (player === 'scissors') {
-        if (computer === 'rock') {
-            return 'You lose! Rock beats scissors!'
-        } else if (computer === 'paper') {
-            return 'You win! Scissors beat paper'
-        } else {
-            return 'Scissors and scissors! It\'s a tie!'
-        }
-    }
-}
-
-console.log(playRound(playerSelection(), computerSelection()));
+console.log(game());
